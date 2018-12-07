42fa131100aa8a06c2aa3e0810a7e326b171e9e4
Lawliet02 <diegoavilapava@hotmail.com>
2018-12-06 23:16:44 -0500

    The Events Module and EventEmitter Class

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-const tutorial = require('./tutorial');
-console.log(tutorial);
-console.log(tutorial.sum(1,2));
-console.log(tutorial.PI);
-console.log(new tutorial.SomeMathObject());
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('tutorial', (num1, num2)=>{
+    console.log(num1 + num2);
+});
+
+eventEmitter.emit('tutorial',1,2);
+
+class Person extends EventEmitter{
+    constructor(name){
+        super();
+        this._name = name;
+    }
+
+    get name(){
+        return this._name;
+    }
+}
+
+let pedro = new Person('Pedro');
+let christina = new Person('Chtristina');
+
+christina.on('name', ()=>{
+    console.log('my name is ' + pedro.name);
+});
+pedro.on('name', ()=>{
+    console.log('my name is ' + christina.name);
+});
+pedro.emit('name');
+christina.emit('name');
