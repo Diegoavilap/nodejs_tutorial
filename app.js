f858ce8af679527f0524a9f2103e76e52f0784da
Lawliet02 <diegoavilapava@hotmail.com>
2018-12-07 22:28:45 -0500

    Working with Readable and Writable Streams

--- app.js
+++ app.js
@@ -1,41 +1,6 @@
 const fs = require('fs');
-//create a folder
-/*
-fs.mkdir('tutorial', (err) => {
-    if(err)
-        console.log(err);
-    else
-        fs.writeFile('./tutorial/example.txt', '123',(err)=>{
-            if(err)
-                console.log(err);
-            else
-                console.log('successfully created file');
-        });
-});
-
-fs.unlink('./tutorial/example.txt', (err)=>{
-    if(err)
-        console.log(err);
-    else
-        console.log('sucessfully the file');
-});
-fs.rmdir('tutorial',(err)=>{
-    if(err)
-        console.log(err)
-    else
-        console.log('deleted folder');
-});
-*/
-fs.readdir('example', (err, files)=>{
-    if(err)
-        console.log(err);
-    else
-        for(let file of files){
-            fs.unlink('./example/' + file, (err)=>{
-                if(err)
-                    console.log(err);
-                else
-                    console.log('successfully deleted file');
-            });
-        }
+const readStream = fs.createReadStream('./example.txt', 'utf8');
+const writeStream = fs.createWriteStream('example2.txt');
+readStream.on('data', (chunk)=>{
+    writeStream.write(chunk);
 });
