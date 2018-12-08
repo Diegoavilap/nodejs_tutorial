7bd2980b3d96aeefb60f250d9e2ad30a54c59ed5
Lawliet02 <diegoavilapava@hotmail.com>
2018-12-07 22:46:08 -0500

    Pipes and Pipe Chaining. (Readable,Writable and Transform Streams)

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
+const zlib = require('zlib');
+const gunzip = zlib.createGunzip();
+/*
+//compressing a file to gzip
 const readStream = fs.createReadStream('./example.txt', 'utf8');
-const writeStream = fs.createWriteStream('example2.txt');
-readStream.on('data', (chunk)=>{
-    writeStream.write(chunk);
-});
+const writeStream = fs.createWriteStream('example2.txt.gz');
+
+readStream.pipe(gzip).pipe(writeStream);
+*/
+
+//Uncompressing a file gzip with gunzip
+const readStream =fs.createReadStream('./example2.txt.gz');
+const writeStream =fs.createWriteStream('uncompressed.txt');
+readStream.pipe(gunzip).pipe(writeStream);
