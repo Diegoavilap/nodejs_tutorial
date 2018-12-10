538dc5292e98b697f59d5d194ee2eae53e4073c3
Lawliet02 <diegoavilapava@hotmail.com>
2018-12-10 18:31:15 -0500

    Installing Packages using Npm (Node Package Manager)

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-const http = require('http');
-const fs = require('fs');
-const server = http.createServer((req, res)=>{
-    const readStream = fs.createReadStream('./static/example.json');
-    // html --> text/html
-    //Image --> image/png
-    //Json --> application/json
-    res.writeHead(200, {'Content-type': 'application/json'});
-    readStream.pipe(res);
-}).listen('3000');
+const _ = require('lodash');
+let example = _.fill([1,2,3,4,5], "banana",1,4);
+console.log(example);
