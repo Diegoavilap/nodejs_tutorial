1a20ba4bea622898d54d287a98a34c5c7c360eb3
Lawliet02 <diegoavilapava@hotmail.com>
2018-12-11 00:17:14 -0500

    User Input Validation With Express And JOI

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const path = require('path');
+const Joi = require('joi');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json())
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname,'static', 'index.html'));
 });
 
 app.post('/',(req, res)=>{
     console.log(req.body);
+    const schema = Joi.object().keys({
+        email: Joi.string().trim().email().required(),
+        password: Joi.string().min(5).max(10).required()
+    });
+    Joi.validate(req.body, schema, (error, result)=>{
+        if (error) {
+            console.log(error);
+            res.send('An error has ocurred');
+        }
+        console.log(result);
+        res.send('sucessfully posted data');
+    });
     //dattabase work here
-    res.json({success: true});
 });
 app.listen(3000);
