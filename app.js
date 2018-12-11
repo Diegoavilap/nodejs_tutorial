e2b81b85f935a960dedbf13c901bf27a3956b89d
Lawliet02 <diegoavilapava@hotmail.com>
2018-12-10 22:59:14 -0500

    Http Post Request with Express and Body Parser Module

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname,'static', 'index.html'));
 });
-app.get('/example', (req, res)=>{
-    res.send('hitting example route');
-});
-app.get('/example/:name/:age', (req, res)=>{
-    console.log(req.query);
-    res.send(req.params.name + ' : ' + req.params.age);
+
+app.post('/',(req, res)=>{
+    console.log(req.body);
+    //dattabase work here
+    res.send('sucessfully posted data ');
 });
 app.listen(3000);
