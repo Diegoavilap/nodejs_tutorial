a2456abfdb07ae3c061b85d5c35b60cd2a4094de
Lawliet02 <diegoavilapava@hotmail.com>
2018-12-11 10:07:29 -0500

    How does MiddleWare Work and Creating Custom Middleware

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const path = require('path');
-//const Joi = require('joi');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use('/public', express.static(path.join(__dirname, 'static')));
-app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use((req,res,next)=>{
+    console.log(req.url, req.method);
+    req.banana = 'banana';
 
-app.get('/:userQuery', (req, res)=>{
-    res.render('index', {data: {userQuery: req.params.userQuery,
-                                searchResults: ['book1', 'book2', 'book3'],
-                                loggedIn: true,
-                                username: "Beto"
-                                }
-
-    });
+    next();
 });
+
+app.get('/', (req,res)=>{
+    console.log(req.banana);
+    res.send('Middleware');
+});
+
+
 
 app.listen(3000);
